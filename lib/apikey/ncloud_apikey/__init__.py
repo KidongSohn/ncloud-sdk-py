1c62471a9bd320d77164ed3193a0ebb9f64229ff
KidongSohn <kidong.sohn@navercorp.com>
2018-06-29 12:39:04 +0900

    ncloud_apikey 수정

--- lib/apikey/ncloud_apikey/__init__.py
+++ lib/apikey/ncloud_apikey/__init__.py
@@ -2,3 +2,4 @@ from __future__ import absolute_import
 
 # import apis into api package
 from ncloud_apikey.ncloud_key import NcloudKey
+from ncloud_apikey.properties_parser y import PropertiesParser
